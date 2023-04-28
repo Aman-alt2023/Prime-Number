a78275ada5f364f936efe4dedf2133edb1766aa3
Aman-alt2023 <132070195+Aman-alt2023@users.noreply.github.com>
2023-04-28 18:11:52 +0530

    How to get prime number

---
+++ Hello.py
@@ -0,0 +1,11 @@
+num = int(input("enter the number"))
+
+if  num %2 == 0:
+    print("its not a prime number")
+elif num == 1:
+    print("its not prime")
+elif num <= 0:
+    print("its not prime")
+else:
+    print("its a prime")
+
